4d5160cc5bf31f3ba01e1faa4ad73dea08ce1ea0
Roger Chin <rogerchin85@gmail.com>
2018-07-17 19:19:34 -0700

    changed database connection config

--- routers/db.js
+++ routers/db.js
@@ -1,10 +1,20 @@
 const pg = require('pg');
+const url = require('url');
 
 let db;
 
 if (process.env.NODE_ENV == 'production') {
+    let params = url.parse(process.env.DATABASE_URL);
+    let auth = params.auth.split(':');
+
     db = new pg.Pool({
-        host: process.env.DATABASE_URL
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/'[1]),
+        max: 20,
+        ssl: true
     });
 } else if (process.env.NODE_DEV === 'development') {
     db = new pg.Pool({
