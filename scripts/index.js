8b9f01abc389fe3b598e26f01ec74a462bc53f66
Roger Chin <rogerchin85@gmail.com>
2018-07-30 01:57:23 -0700

    showing first category on index

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
     $('.category-bar').on('click', function() {
-        $(this).siblings('.category-list-parent').slideToggle();
+        $(this).siblings('section').slideToggle();
+
+        if ($(this).children('i').hasClass('fa-angle-up')) {
+            $(this).children('i').removeClass('fa-angle-up').addClass('fa-angle-down');
+        } else {
+            $(this).children('i').removeClass('fa-angle-down').addClass('fa-angle-up');
+        }
     });
 
     $('.category-title').on('click', function(e) {
